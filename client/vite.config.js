1878fab0a6b871f63fcf8fffae8ab02302d6e44a
Ryan Herrington <RyanPHerrington@gmail.com>
2023-09-30 19:47:22 -0400

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig(({ command, mode }) => {
 
     server: {
       host: true,
-      port : 3000,
+      port : 2121,
       proxy: {
         '/api': {
           target: env.VITE_PROXY,
