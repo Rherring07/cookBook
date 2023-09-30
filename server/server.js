bca5b2214b04b9cbf0d9f91de27cd1fcc1c09030
Ryan Herrington <RyanPHerrington@gmail.com>
2023-09-30 18:51:43 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -30,10 +30,10 @@ const port = process.env.PORT || 5000;
 require("dotenv").config({ path: "./config/.env" });
 
 //Cyclic Hosting Frontend and Backend
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "../client/dist")));
 app.get("*", function (_, res) {
   res.sendFile(
-    path.join(__dirname, "./client/build/index.html"),
+    path.join(__dirname, "../client/build/dist"),
     function (err) {
       res.status(500).send(err);
     }
